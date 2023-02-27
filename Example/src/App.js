b8fb7a540c68f45821b126b0a7a11def299cfda4
anis-bougrine <anis.bougrine10@gmail.com>
2023-02-27 01:53:39 +0100

    Example enrichment + Readme.md update

--- Example/src/App.js
+++ Example/src/App.js
@@ -1,8 +1,14 @@
 import {click_scrolling, smart_scrolling} from "./Smart_Scrolling";
-import $ from "jquery";
 import logo from './logo.svg';
 import './App.css';
+import text from "./long_text.js";
 
+/**
+* Render <p> element containing description.
+*
+* @param {undefined} undefined.
+* @returns {undefined} undefined.
+*/
 function Description(){
   return (
       <p>
@@ -12,13 +18,26 @@ function Description(){
   );
 }
 
+/**
+* render scrolling element can be cutomized by props.
+*
+* @param {object} props - { smart: false, dynamic: false }
+* @returns {undefined} undefined.
+*/
 function ToScroll(props){
-
-  if (props.smart){
-    return(
-      <div className="bloc">
-      <div id="prev" onClick={() => {click_scrolling("box1", "left");}}></div>
-      <div id="box1"  onScroll={() =>{smart_scrolling("box1");}}>
+  return(
+    <div className={!props.dynamic?("bloc"):("")} id={props.dynamic?("bloc-text"):("")}>
+      {props.smart ? (
+    <div className="prev" onClick={props.dynamic?(() => {click_scrolling("long_text", "left");}):(() => {click_scrolling("box1", "left");})}></div>
+      ):(
+      <div></div>
+      )}
+    <div id={props.smart ? ("box1"):("box2")}  onScroll={props.smart ? (() =>{smart_scrolling("box1");}):(()=>{})}>
+      {props.dynamic?(
+        <p id="long_text" onScroll={() =>{smart_scrolling("long_text");}}>
+          {text}
+        </p>
+      ):(
         <div className="movingbox1">
           <div className="page1">
             <p className="pages">PAGE X</p>
@@ -30,32 +49,27 @@ function ToScroll(props){
             <p className="pages">PAGE Z</p>
           </div>
         </div>
-      </div>
-      <div id="next" onClick={() => {click_scrolling("box1", "right");}}></div>
+      )}
     </div>
-    );
-  } else if (!props.smart){
-    return(
-      <div className="bloc">
-      <div id="box2">
-        <div className="movingbox1">
-          <div className="page1">
-            <p className="pages">PAGE X</p>
-          </div>
-          <div className="page2" >
-            <p className="pages" >PAGE Y</p>
-          </div>
-          <div className="page3">
-            <p className="pages">PAGE Z</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    );
-  }
-
+    {props.smart ? (
+    <div className="next" onClick={props.dynamic?(() => {click_scrolling("long_text", "right");}):(() => {click_scrolling("box1", "right");})}></div>
+    ):(
+      <div></div>
+      )}
+  </div>
+  );
 }
+ToScroll.defaultProps = {
+  smart:false,
+  dynamic: false
+};
 
+/**
+* Render 3 scrolling elements, each is specific by its props.
+*
+* @param {undefined} undefined.
+* @returns {undefined} undefined.
+*/
 function App() {
   return (
     <div className="App">
@@ -67,9 +81,14 @@ function App() {
         </p>
         <ToScroll smart={false}/>
         <p>
-          This is smart scroll effect:
+          This is smart scroll effect for static overflow:
         </p>
         <ToScroll smart={true}/>
+        <p>
+          This is smart scroll effect for dynamic overflow:
+        </p>
+        <ToScroll smart={true} dynamic={true}/>
+        <br></br>
       </header>
     </div>
   );
--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 ## Description
 - A generic light weight and smart horizontal scrolling function, supports button scrolling and touch scrolling.
 - When scrolling an horizontal overflow element in a html page, this function going to figure out how many pages the element contains, where the size of the page is the element offsetwidth, and scrolling back and forward going to switch between pages.
+- The power of this function can be seen when facing a dynamic overflow size, for example a text element where font-size variation can shrink or expand the overflow, this function can handle it, check the example section for a demo.
 - Scrolling with button clicking and screen touching is synchronized.
 - Particular cases where number of pages is a float not an integer, which leads to an incomplete page at the last page, is supported.
+- This function can be easly transformed to vertical scrolling.
 ## Example running
 An example of React application implementing the smart sroll function is provided to give you an idea on how it can be used. To see how it works please follow this steps:
 ```
@@ -13,6 +15,6 @@ npm install react-scripts
 npm start
 ```
 
-Executing this command will open a html page like the image below and now it's up to you to enjoy scrolling :)
+Executing this commands will open a html page like the capture below, now it's up to you to enjoy scrolling :)
 
 ![Alt Text](./Example/Example_capture.png "Optional Title")
