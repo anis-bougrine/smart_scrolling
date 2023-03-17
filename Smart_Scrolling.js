f842f2cf437105fc1ae92d5955a6b55bdf1c7b52
Bougrine Anis <58916821+anis-bougrine@users.noreply.github.com>
2023-03-17 12:56:05 +0100

    Add timeout condition to while loops statement

--- Smart_Scrolling.js
+++ Smart_Scrolling.js
@@ -1,3 +1,7 @@
+const temp = 1;
+const pixel_by_temp = 3;
+const timeout = 1000;
+
 /**
  * waits a specific number of ms.
  *
@@ -9,7 +13,7 @@ function wait(ms) {
 }
 
 /**
-* scrolls 10 pixels in a specific direction (left or right).
+* scrolls n pixels in a specific direction (left or right).
 *
 * @param {string} element - id of JSX element.
 * @param {string} direction - direction of scroll (default:"right").
@@ -17,10 +21,10 @@ function wait(ms) {
 */
 function scrolling(element, direction="right"){
   if (direction === "right"){
-    element.scrollLeft += 10;
+    element.scrollLeft += pixel_by_temp;
   }
   else if (direction === "left"){
-    element.scrollLeft -= 10;
+    element.scrollLeft -= pixel_by_temp;
   }
   else{
     console.error("no direction setted");
@@ -36,34 +40,48 @@ function scrolling(element, direction="right"){
 */
 async function click_scrolling(element, direction){
 
+  let shouldBreak = false;
+
+  setTimeout(() => {
+    shouldBreak = true;
+  }, timeout); 
+
     const long_element = document.getElementById(element);
 
-    const box_size = long_element.offsetWidth;
+    let column_gap = window.getComputedStyle(long_element).getPropertyValue("column-gap");
+    if (column_gap !== "normal"){
+      column_gap = parseInt(column_gap);
+    }else{
+      column_gap = 0;
+    }
+
+    const box_size = long_element.offsetWidth + column_gap;
+    const scrollWidth = long_element.scrollWidth + column_gap;
     let current_position = Math.round(long_element.scrollLeft / box_size)+1;
-    console.log(current_position);
 
     //scroll right
     if ( direction === "right"){
       //scroll until next page
-      while( (long_element.scrollLeft < (((current_position-1) * box_size) + box_size / 5 + 1)) & (long_element.scrollLeft != long_element.scrollWidth-box_size) ){
+      while( !shouldBreak & (long_element.scrollLeft < (((current_position-1) * box_size) + box_size / 5 + 1))){
         scrolling(long_element, direction);
-        await wait(10);
+        await wait(temp);
       }
     }
     //scroll left
     else if ( direction === "left"){
       //Before while loop we calibrate page position (this is a specific case, if last page is reached and its size is less than 0.5*page_size)
-      if (long_element.scrollWidth%box_size <= 0.5*box_size & long_element.scrollWidth - box_size <= long_element.scrollLeft+10 ){
+      if (scrollWidth%box_size <= 0.51*box_size & scrollWidth - box_size <= long_element.scrollLeft ){
         current_position++;
       }
-      //scroll until prevuois page
-      while( ((current_position-1) * box_size) - box_size / 5 - 1 < long_element.scrollLeft & (long_element.scrollLeft != 0)){
+      //scroll until previous page
+      while( !shouldBreak & ((current_position-1) * box_size) - box_size / 5 - 1 < long_element.scrollLeft){
         scrolling(long_element, direction);
-        await wait(10);
+        await wait(temp);
       }
       //we check if we reached last page and we didn't enter while loop (this is a specific case, if last page size is less than 4/5 page_size)
-      if(long_element.scrollWidth - box_size <= long_element.scrollLeft+10){
+      if(scrollWidth - box_size <= long_element.scrollLeft){
         scrolling(long_element, direction); 
+        // click_scrolling(element, direction)
       }
     }
 }
@@ -76,8 +94,21 @@ async function click_scrolling(element, direction){
 */
 async function smart_scrolling(element) {
 
+  let shouldBreak = false;
+
+  setTimeout(() => {
+    shouldBreak = true;
+  }, timeout); 
+
   const long_element = document.getElementById(element);
-  
+  const paddingLeft = parseInt(window.getComputedStyle(long_element).getPropertyValue("padding-left"));
+  let column_gap = window.getComputedStyle(long_element).getPropertyValue("column-gap");
+  if (column_gap !== "normal"){
+    column_gap = parseInt(column_gap);
+  }else{
+    column_gap = 0;
+  }
+
   //Static variables
   if ( typeof smart_scrolling.prevscroll == 'undefined' ) {
       smart_scrolling.prevscroll = 0 ;
@@ -91,26 +122,30 @@ async function smart_scrolling(element) {
   const delta_time = new Date() - smart_scrolling.last_scroll;
   smart_scrolling.last_scroll = new Date();
 
-  const box_size = long_element.offsetWidth;
+  const box_size = long_element.offsetWidth+column_gap;
+  const scrollWidth = long_element.scrollWidth + column_gap;
   let current_position = Math.round(long_element.scrollLeft / box_size)+1;
+
+  console.log(current_position);
 
   //scrolling right
   if(delta_scroll > 0){
     //(i)==>(i+1)
-    while(((current_position-1)*box_size + box_size/ 5) < long_element.scrollLeft & long_element.scrollLeft < current_position * box_size & long_element.scrollLeft != (long_element.scrollWidth - box_size)){
+    while(!shouldBreak & ((current_position-1)*box_size + box_size/ 5) < long_element.scrollLeft & long_element.scrollLeft < current_position * box_size - paddingLeft){
       scrolling(long_element, "right");
-      await wait(10);
+      await wait(temp);
     }
   //scrolling left
   }else if (delta_scroll < 0){
     //before while loop we calibrate page position (this is a specific case where we reach last page and its size is less than 0.5 page_size)
-    if (long_element.scrollWidth%box_size <= 0.5*box_size & long_element.scrollWidth - box_size <= long_element.scrollLeft+10 ){
+   
+    if (scrollWidth%box_size <= 0.51*box_size & scrollWidth - box_size <= long_element.scrollLeft+10 ){
       current_position++;
     }
     //(i)==>(i-1)
-    while((current_position-2)*box_size < long_element.scrollLeft & (long_element.scrollLeft < (current_position-1) * box_size - box_size/5) & long_element.scrollLeft != 0 ){
+    while(!shouldBreak & (current_position-2)*box_size - paddingLeft < long_element.scrollLeft & (long_element.scrollLeft < (current_position-1) * box_size - box_size/5)){
       scrolling(long_element, "left");
-      await wait(10);
+      await wait(temp);
     }
 
   }
